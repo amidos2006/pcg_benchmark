74a2dad2c592036695fb2fc5543328246448e4e7
gallorob <32876132+gallorob@users.noreply.github.com>
2024-10-21 09:05:45 +0200

    add to_latex and other bar plots

--- data_processing.py
+++ data_processing.py
@@ -238,6 +238,8 @@ def create_line_plots(csv_file, output_dir):
 	# Load the aggregated statistics from the CSV file
 	df = pd.read_csv(csv_file)
 	
+	df = df.loc[df['fitness_type'] == 'quality']
+	
 	# Ensure the output directory exists
 	os.makedirs(output_dir, exist_ok=True)
 	
@@ -248,25 +250,28 @@ def create_line_plots(csv_file, output_dir):
 	for env_name, env_group in df.groupby('env_name'):
 		
 		# Iterate over each property to plot
-		for prop in ['fitness', 'quality', 'diversity', 'controlability']:
+		for prop in ['fitness']:#, 'quality', 'diversity', 'controlability']:
 			plt.figure(figsize=(12, 8))
-			sns.lineplot(
+			lineplot = sns.lineplot(
 				data=env_group,
 				x='iter_n',
 				y=f'mean_{prop}',
 				hue='algorithm',
 				hue_order=['random', 'ga', 'es'],  # Custom sorting of the algorithms
-				style='fitness_type',
+				#style='fitness_type',
 				markers=False,
 				dashes=True,
 				errorbar=None
 			)
-			for key, grp in env_group.groupby(['fitness_type', 'algorithm']):
+			#for key, grp in env_group.groupby(['fitness_type', 'algorithm']):
+			for key, grp in env_group.groupby(['algorithm']):
+				color = lineplot.get_lines()[['random', 'ga', 'es'].index(key[0])].get_color()
 				plt.fill_between(
 					grp['iter_n'],
 					grp[f'lower_ci_{prop}'],
 					grp[f'upper_ci_{prop}'],
 					alpha=0.2,
+					color=color,
 					label=None#f'{key[0]} - {key[1]} {prop.title()} CI'
 				)
 				
@@ -320,10 +325,13 @@ def render_elites(csv_file, output_dir):
 	
 	df = pd.read_csv(csv_file)
 	folder_path = os.path.join(csv_file, '..')  # This is a hack, I know, but it works
-	
+		
 	for env_name in df['env_name'].unique():
 		for fitness_type in df['fitness_type'].unique():
 			for algorithm in df['algorithm'].unique():
+				elites = []
+				fitnesses = []
+				runs = []
 				for run_n in df['run_n'].unique():
 					print(f'Processing {env_name} ({fitness_type} - {algorithm} - {run_n})')
 					run_data = df.loc[
@@ -332,30 +340,206 @@ def render_elites(csv_file, output_dir):
 						(df['algorithm'] == algorithm) &
 						(df['run_n'] == run_n)]
 					last_iter = max(run_data['iter_n'])
-					latest_elite_file = run_data.loc[run_data['iter_n'] == last_iter]['elite'].values[0]
+					elites.append(run_data.loc[run_data['iter_n'] == last_iter]['elite'].values[0])
+					fitnesses.append(run_data.loc[run_data['iter_n'] == last_iter]['fitness'].values[0])
+					runs.append(run_n)
 					
-					with open(f'./results/{env_name}/{fitness_type}/{algorithm}/{run_n}/iter_{last_iter}/{latest_elite_file}', 'r') as f:
+				idxs = np.argsort(fitnesses)[::-1]
+				elites_sorted = [elites[idx] for idx in idxs]
+				fitnesses_sorted = [fitnesses[idx] for idx in idxs]
+				runs_sorted = [runs[idx] for idx in idxs]
+				
+				for i, (elite, fitness, run_n) in enumerate(zip(elites_sorted, fitnesses_sorted, runs_sorted)):
+					with open(f'{folder_path}/{env_name}/{fitness_type}/{algorithm}/{run_n}/iter_{last_iter}/{elite}', 'r') as f:
 						latest_elite = json.load(f)
 					
 					env = pcg_benchmark.make(env_name)
+					str_fitness = '__' + str(fitness).replace('.', '_') + '__'
 					
 					if env_name != 'talakat-v0':
-						env.render(latest_elite['content']).save(os.path.join(output_dir, f'{env_name}_{fitness_type}_{algorithm}_{run_n}_elite.png'))
+						env.render(latest_elite['content']).save(os.path.join(output_dir, f'{env_name}_{fitness_type}_{algorithm}_n{i}{str_fitness}{run_n}_elite.png'))
 					else:
 						frames = env.render(latest_elite['content'])
-						frames[0].save(os.path.join(output_dir, f'{env_name}_{fitness_type}_{algorithm}_{run_n}_elite.gif'), append_images=frames[1:], save_all=True, duration=100, loop=0)
+						frames[0].save(os.path.join(output_dir, f'{env_name}_{fitness_type}_{algorithm}_n{i}{str_fitness}{run_n}_elite.gif'), append_images=frames[1:], save_all=True, duration=100, loop=0)
+					
+					if i == 2: break
+
+						
+def to_latex_table(csv_file,
+				   metric,
+				   output_dir):
+	env_names_latex = [
+		"Arcade Rules", "Binary", "Building", "Dangerous Dave", "Elimination", 
+		"The Binding of Isaac", "Lode Runner", "Mini Dungeons", "Super Mario Bros", 
+		"Sokoban", "Talakat", "Zelda"
+	]
+
+	env_names_df = [
+		"arcade-v0", "binary-v0", "building-v0", "ddave-v0", "elimination-v0", 
+		"isaac-v0", "loderunner-v0", "mdungeons-v0", "smb-v0", 
+		"sokoban-v0", "talakat-v0", "zelda-v0"
+	]
+
+	algorithms_latex = ['Random Search', '$\mu + \lambda$ Evolution Strategy', 'Genetic Algorithm']
+	algorithms_df = ['random', 'es', 'ga']
+
+	# Initialize LaTeX table
+	latex_code = r'''
+	\begin{table*}
+		\centering
+		\begin{tabular}{|l||c|c|c|c|c|c|c|c|c|}
+			\hline
+			& \multicolumn{3}{|c|}{Random Search} & \multicolumn{3}{|c|}{$\mu + \lambda$ Evolution Strategy} & \multicolumn{3}{|c|}{Genetic Algorithm} \\\cline{2-10}
+			& Q & QC & QCD & Q & QC & QCD & Q & QC & QCD \\
+			\hline \hline
+	'''
+
+	df = pd.read_csv(csv_file)
 	
+	# Iterate through environment names and append data
+	for env_name_latex, env_name_df in zip(env_names_latex, env_names_df):
+		# Initialize row with environment name
+		row = f'        {env_name_latex}'
+
+		means = []
+		ci_diffs = []
+
+		# Fetch data for the environment and different algorithms
+		for algo in algorithms_df:
+			for fitness in ['quality', 'quality_control', 'quality_control_diversity']:
+				# Filter the dataframe for the current environment and algorithm
+				row_data = df[(df['env_name'] == env_name_df) & 
+								(df['algorithm'] == algo) &
+								(df['fitness_type'] == fitness) & 
+								(df['iter_n'] == max(df['iter_n'].unique()))
+								]
+				mean_value = row_data[f'mean_success_{metric}'].values[-1]
+				means.append(mean_value)
+				ci_diff_value = row_data[f'upper_ci_success_{metric}'].values[-1]
+				ci_diffs.append(ci_diff_value)
+				
+		max_mean_value = np.max(means)
+
+		for (mean, ci_diff) in zip(means, ci_diffs):
+			if mean == max_mean_value:
+				row += '& \\textbf{' + f'{mean:.2f}' + '}'
+				if not np.isnan(ci_diff):
+					row += '$\\mathbf{\\pm}$\\textbf{' + f'{(ci_diff - mean):2f}' + '}'
+			else:
+				row += f'& {mean:.2f}'
+				if not np.isnan(ci_diff):
+					row += f'$\\pm${(ci_diff - mean):2f}'
+
+		# Close the row
+		row += r' \\\hline'
 		
-			
-# Define a class to wrap both functions
+		# Append row to LaTeX table
+		latex_code += row + '\n'
+
+	# Close the table
+	latex_code += r'''
+		\end{tabular}
+		\caption{This table shows the number of individuals in the final generation that pass the benchmark criteria for the \textit{controllability} metric for every different problem. The reported value is the mean between 10 runs followed by the 95\% confidence interval.}
+		\label{tab:benchmark_control}
+	\end{table*}
+	'''
+
+	with open(os.path.join(output_dir, f'{metric}.tex'), 'w') as f:
+		f.write(latex_code)
+
+
+def compute_runs_diversity(csv_file,
+						   output_dir):
+	import pcg_benchmark
+	df = pd.read_csv(csv_file)
+
+	folder_path = os.path.join(csv_file, '..')  # This is a hack, I know, but it works
+	
+	matplotlib.rcParams.update({'font.size': 30})
+	
+	plot_data = pd.DataFrame(columns=[
+		'fitness_type', 'env_name', 'algorithm', 'elites_diversity', 'mean_success'
+	])
+	
+	for fitness_type in tqdm(df['fitness_type'].unique(), desc='Fitness type', leave=False, dynamic_ncols=True):
+		for env_name in tqdm(df['env_name'].unique(), desc='Environment', leave=False, dynamic_ncols=True):
+
+			env = pcg_benchmark.make(env_name)
+
+			for algorithm in tqdm(df['algorithm'].unique(), desc='Algorithm', leave=False, dynamic_ncols=True):
+				elites = []
+				n_success_quality = []
+				for run_n in tqdm(df['run_n'].unique(), desc='Run number', leave=False, dynamic_ncols=True):
+					run_data = df.loc[
+						(df['env_name'] == env_name) &
+						(df['fitness_type'] == fitness_type) &
+						(df['algorithm'] == algorithm) &
+						(df['run_n'] == run_n)]
+					last_iter = max(run_data['iter_n'])
+					elite_fname = run_data.loc[run_data['iter_n'] == last_iter]['elite'].values[0]
+					with open(f'{folder_path}/{env_name}/{fitness_type}/{algorithm}/{run_n}/iter_{last_iter}/{elite_fname}', 'r') as f:
+						elite = json.load(f)
+					elites.append(elite['content'])
+					n_success_quality = run_data.loc[run_data['iter_n'] == last_iter]['success_quality'].values[0]
+				diversity = env.diversity(elites)[0] * 100
+				mean_success = np.mean(n_success_quality)
+				plot_data = plot_data._append({'fitness_type': fitness_type, 'env_name': env_name, 'algorithm': algorithm, 'elites_diversity': diversity, 'mean_success': mean_success}, ignore_index=True)
+		
+	fitness_types = plot_data['fitness_type'].unique()
+
+	for fitness in fitness_types:
+		fitness_data = plot_data[plot_data['fitness_type'] == fitness]
+
+		plt.figure(figsize=(12, 8))
+		sns.barplot(x='env_name', y='elites_diversity',
+					hue='algorithm', hue_order=['random', 'ga', 'es'],  # Custom sorting of the algorithms,
+					data=fitness_data)
+		
+		plt.title('')
+		plt.xlabel('Problems')
+		plt.xticks(rotation=45, ha='right')
+		plt.ylabel('% Unique Individuals')
+		plt.yticks([0, 25, 50, 75, 100])
+		
+		plt.legend([],[],frameon=False)
+		plt.tight_layout()
+		
+		plot_filename = f"diversity_{fitness}.png"
+		plt.savefig(os.path.join(output_dir, plot_filename))
+
+	for fitness in fitness_types:
+		fitness_data = plot_data[plot_data['fitness_type'] == fitness]
+
+		plt.figure(figsize=(12, 8))
+		sns.barplot(x='env_name', y='mean_success',
+					hue='algorithm', hue_order=['random', 'ga', 'es'],  # Custom sorting of the algorithms,
+					data=fitness_data)
+		
+		plt.title('')
+		plt.xlabel('Problems')
+		plt.xticks(rotation=45, ha='right')
+		plt.ylabel('Mean % Success')
+		plt.yticks([0, 25, 50, 75, 100])
+		
+		plt.legend([],[],frameon=False)
+		plt.tight_layout()
+		
+		plot_filename = f"success_{fitness}.png"
+		plt.savefig(os.path.join(output_dir, plot_filename))
+		
+	
 class DataCruncher:
 	def run_pipeline(self,
 	                 root_dir,
-	                 output_dir):
+	                 output_dir,
+					 renders_dir,
+					 latex_dir):
 		fnames = self.process_all_envs(root_dir)
 		raw_csv = self.aggregate_envs_statistics(root_dir, fnames)
 		aggr_csv = self.aggregate_over_runs(raw_csv)
 		self.plot(aggr_csv, output_dir)
+		self.render_elites(raw_csv, renders_dir)
+		self.to_latex(aggr_csv, latex_dir)
 	
 	def process_environment(self, env_path):
 		process_environment(env_path)
@@ -372,8 +556,15 @@ class DataCruncher:
 	def plot(self, csv_file, output_dir):
 		create_line_plots(csv_file, output_dir)
 	
-	def render_elites(self, csv_file, output_dir):
-		render_elites(csv_file, output_dir)
+	def render_elites(self, csv_file, renders_dir):
+		render_elites(csv_file, renders_dir)
+	
+	def to_latex(self, csv_file, latex_dir):
+		for metric in ['quality', 'controlability', 'diversity']:
+			to_latex_table(csv_file, metric, latex_dir)
+	
+	def compute_runs_diversity(self, csv_file, output_dir):
+		compute_runs_diversity(csv_file, output_dir)
 	
 
 if __name__ == "__main__":
