dfb767bf3fedcd5e99c263c6e3abd1fa662c2b08
amidos2006 <amidos2002@hotmail.com>
2025-01-08 15:13:06 +0100

    fixing the bug with packages

--- setup.py
+++ setup.py
@@ -17,6 +17,5 @@ setup(name='pcg_benchmark',
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-      ],
-      packages=['pcg_benchmark'],
+      ]
 )
